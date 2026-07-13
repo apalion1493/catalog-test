253ce237a10ed897d3ff0ff971ecba4f0b329152
Alexandr Petrovich <apalion97@gmail.com>
2026-07-13 15:07:12 +0300

    first

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+// import { useState } from 'react'
+import './App.css'
+import {Header} from "./components/Header.tsx";
+import {Footer} from "./components/Footer.tsx";
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Header/>
+      <body>
+      <h1>Hello Vite + React!</h1>
+      </body>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <footer className={"mt-auto"}>
+      <h3>Footer</h3>
+    </footer>
+  )
+}
